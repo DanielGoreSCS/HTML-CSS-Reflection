70e14386e1e43778a878a89a2b87c4ef30046320
Daniel Gore <daniel.gore@netmatters-scs.com>
2022-07-22 09:43:23 +0100

    Added the mobile menu
    I forgot to commit. The mobile menu has been added. I have added a image carousel.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $hamburger = $(".hamburger");
+$wrapper = $(".wrapper");
+$wrapperFade = $(".fade-wrapper");
 $hamburger.on("click", () => {
     $hamburger.toggleClass("is-active");
+    $(".sidebar").toggleClass("show");
+    $wrapperFade.toggleClass("show slideLeft");
+    $wrapper.toggleClass("slideLeft");
+    $("body").css({"overflow":"hidden"});
+    $wrapper.css({"overflow":"scroll"});
+});
+$wrapperFade.on("click", () => {
+    $hamburger.toggleClass("is-active");
+    $(".sidebar").toggleClass("show");
+    $wrapperFade.toggleClass("show slideLeft");
+    $wrapper.toggleClass("slideLeft");
+    $("body").removeAttr("style");
+    $wrapper.removeAttr("style");
 });
 
 $(".nav-item").hover(function () {
@@ -8,3 +23,15 @@ $(".nav-item").hover(function () {
 }, function () {
     $(this).children().removeClass('show');
 });
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin: 100,
+        items: 8,
+        autoWidth: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        loop: true
+    });
+  });
