ebd5bf32b0e7665f2e7d3b86c37d957e11ef7b12
Daniel Gore <daniel.gore@netmatters-scs.com>
2022-08-01 17:23:53 +0100

    Added sticky header, adding basic cookie popup
    Uploading for review, I have not included the advanced menu for the cookie popup.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $hamburger = $(".hamburger");
 $wrapper = $(".wrapper");
 $wrapperFade = $(".fade-wrapper");
+//lazy fix to cookie bg will change later
+let toggle = 0;
 $hamburger.on("click", () => {
     $hamburger.toggleClass("is-active");
     $(".sidebar").toggleClass("show");
@@ -8,20 +10,40 @@ $hamburger.on("click", () => {
     $wrapper.toggleClass("slideLeft");
     $("body").css({"overflow":"hidden"});
     $wrapper.css({"overflow":"scroll"});
+    toggle = 1;
 });
 $wrapperFade.on("click", () => {
-    $hamburger.toggleClass("is-active");
-    $(".sidebar").toggleClass("show");
-    $wrapperFade.toggleClass("show slideLeft");
-    $wrapper.toggleClass("slideLeft");
-    $("body").removeAttr("style");
-    $wrapper.removeAttr("style");
+    if (toggle != 0) {
+        $hamburger.toggleClass("is-active");
+        $(".sidebar").toggleClass("show");
+        $wrapperFade.toggleClass("show slideLeft");
+        $wrapper.toggleClass("slideLeft");
+        $("body").removeAttr("style");
+        $wrapper.removeAttr("style");
+        toggle = 0;
+    }
 });
 
 $(".nav-item").hover(function () {
     $(this).children().addClass('show');
 }, function () {
     $(this).children().removeClass('show');
+});
+
+//Sticky
+var lastScrollTop = 0;
+$header = $("header");
+$(window).scroll(function(event){
+   var st = $(this).scrollTop();
+   if (st < lastScrollTop){
+       // upscroll code
+       $header.addClass("sticky-top slideDown");
+   } else {
+      // downscroll code
+      $header.addClass("slideUp");
+      setTimeout(() => $header.removeClass("sticky-top slideDown slideUp"),400);
+   }
+   lastScrollTop = st;
 });
 
 $(document).ready(function(){
@@ -35,3 +57,45 @@ $(document).ready(function(){
         loop: true
     });
   });
+
+//cookie popup
+const cookieStorage = {
+    getItem: (key) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce((acc, [key, value]) => ({...acc, [key.trim()]: value}), {});
+        return cookies[key];
+    },
+    setItem: (key, value) => {
+        document.cookie = `${key}=${value}`;
+    },
+};
+
+const storageType = cookieStorage;
+const consentPropertyName = 'jdc_consent';
+
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+    const consentPopup = $("#consent-popup");
+    const acceptBtn = $("#accept");
+
+    const acceptFn = event => {
+        saveToStorage(storageType);
+        consentPopup.addClass("d-none");
+        $wrapperFade.removeClass("show darken");
+        $("body").removeAttr("style");
+        $wrapper.removeAttr("style");
+    }
+
+    acceptBtn.on("click", acceptFn);
+
+    if(shouldShowPopup()) {
+        consentPopup.removeClass("d-none");
+        $wrapperFade.addClass("show darken");
+        $("body").css({"overflow":"hidden"});
+        $wrapper.css({"overflow":"scroll"});
+    }
+}
